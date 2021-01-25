33cb6b9360e24744eeb0a1c96b409256829e16cb
gaurangrshah <gaurang.r.shah@gmail.com>
2021-01-25 12:01:03 -0500

    chore: update nav links ++ secret test page

--- pages/secret.js
+++ pages/secret.js
@@ -1,22 +1,19 @@
 // pages/secret.js
 
-import React from 'react'
-import { useAuth } from '@/hooks/useAuth';
-
+import React from "react";
+import { useAuth } from "@/hooks/useAuth";
 
 const Secret = () => {
   const { isAuthenticated, session, loading, signIn, signOut } = useAuth();
-  if (loading) return "authorizing..."
-  if (!loading && !isAuthenticated) return <p>Access Denied</p>;
 
   return (
-    <div>
+    <div style={{ paddingTop: "12em", margin: "0 auto", textAlign: "center" }}>
       {/* {(session.user.email && session.user.name) && ( */}
-      { isAuthenticated && (
-        process.env.SECRET
-      )}
+      {loading && "authorizing..."}
+      {!loading && !isAuthenticated && "Access Denied"}
+      {!loading && isAuthenticated && process.env.NEXT_PUBLIC_SECRET}
     </div>
-  )
-}
+  );
+};
 
-export default Secret
+export default Secret;
